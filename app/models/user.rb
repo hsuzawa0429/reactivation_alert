5c481c457defcaf9b7880e12b112722f81f58f7b
hsuzawa0429 <hiroyuki.suzawa0429@gmail.com>
2021-09-04 12:01:29 +0900

    userモデル単体テストコード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
-    validates :facility_code, format: { with: /\A[a-zA-Z0-9]+\z/ }
+    validates :facility_code, inclusion: { in: ["KI200404"]}
     validates :user_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-    validates :email, {format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: { case_sensitive: false }}
-    validates :password, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/ }
     validates :affiliation
   end
-  validates :facility_code, inclusion: { in: ["KI200404"]}
+  validates :password, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/ }
+  validates :email, uniqueness: true
 end
