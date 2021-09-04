860b53b0132c2f639e595b67e12e0ff1c8df56f4
hsuzawa0429 <hiroyuki.suzawa0429@gmail.com>
2021-09-04 11:00:27 +0900

    validationの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
-    validates :facility_code
+    validates :facility_code, format: { with: /\A[a-zA-Z0-9]+\z/ }
     validates :user_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-    validates :email
+    validates :email, {format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: { case_sensitive: false }}
     validates :password, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/ }
     validates :affiliation
   end
+  validates :facility_code, inclusion: { in: ["KI200404"]}
 end
