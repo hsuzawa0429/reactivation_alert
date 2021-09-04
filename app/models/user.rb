fe52fc4d3435ed456b64c52a4be85b56542eecb7
hsuzawa0429 <hiroyuki.suzawa0429@gmail.com>
2021-09-04 10:15:34 +0900

    テストコード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :facility_code
+    validates :user_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+    validates :email
+    validates :password, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/ }
+    validates :affiliation
+  end
 end
