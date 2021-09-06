e52df7a5b289ac75011847c3eadfc0a44de3bca0
hsuzawa0429 <hiroyuki.suzawa0429@gmail.com>
2021-09-06 20:13:35 +0900

    patient modelの単体テストコード実装

--- spec/factories/patients.rb
+++ spec/factories/patients.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :patient do
-    
+    patient_code           {"A012345"}
+    gender_id              {'2'}
+    age_id                 {'2'}
+    department_id          {'2'}
+    drug_id                {'2'}
+    monitoring_interval_id {'2'}
+    inspection_date        {"2021-09-06"}
   end
 end
--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Patient, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @patient = FactoryBot.build(:patient)
+  end
+
+  describe '患者新規登録' do
+    it 'patient_codeが空だと登録できない' do
+      @patient.patient_code = ''
+      @patient.valid?
+      expect(@patient.errors.full_messages).to include("Patient code can't be blank")
+    end
+
+    it 'department_idが空だと登録できない' do
+      @patient.department_id = ''
+      @patient.valid?
+      expect(@patient.errors.full_messages).to include("Department can't be blank")
+    end
+
+    it 'monitoring_interval_idが空では登録できない' do
+      @patient.monitoring_interval_id = ''
+      @patient.valid?
+      expect(@patient.errors.full_messages).to include("Monitoring interval can't be blank")
+    end
+
+    it 'inspection_dateが空では登録できない' do
+      @patient.inspection_date = ''
+      @patient.valid?
+      expect(@patient.errors.full_messages).to include("Inspection date can't be blank")
+    end
+  end
 end
